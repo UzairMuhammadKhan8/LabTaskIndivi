a3864e4cb5771b609649d101876a4cf0b213e6b6
your <UK108@live.mdx.ac.uk>
2021-03-17 23:33:35 +0400

    update new things

--- products.js
+++ products.js
@@ -1,7 +1,7 @@
 let arr = [
     {
       "id": 1001,
-      "subject": "Cat Food ddd",
+      "subject": "Cat Food",
       "description": "A 25 pound bag of irresistible, organic goodness for your cat.",
       "location": "DUBAI!",
       "price": 20.00,
@@ -10,49 +10,50 @@ let arr = [
       "rating": 2
     },
     {
+      "id": 1003,
+      "subject": "Royal Canin",
+      "description": "Royal Canin - Indoor 27 (10kg).!",
+      "location": "DUBAI!",
+      "price": 11.00,
+      "image": "C://Users/hp/LabTaskIndivi/images/royal.jpg",
+      "availableInventory": 99,
+      "rating": 4
+    },
+    {
       "id": 1002,
       "subject": "Yarn",
       "description": "Yarn your cat can play with for a very long time!",
       "location": "DUBAI!",
       "price": 2.99,
-      "image": "C://Users/hp/firsttask-git/catimg.jpg",
+      "image": "C://Users/hp/LabTaskIndivi/images/catimg.jpg",
       "availableInventory": 7,
       "rating": 3
     },
     {
-      "id": 1003,
-      "subject": "Kitty Litter",
-      "description": "Premium kitty litter for your cat.",
-      "location": "DUBAI!",
-      "price": 11.00,
-      "image": "C://Users/hp/firsttask-git/catimg.jpg",
-      "availableInventory": 99,
-      "rating": 4
-    },
-    {
       "id": 1004,
-      "subject": "Cat House",
-      "description": "A place for your cat to play!",
+      "subject": "Multi Cat",
+      "description": "A place for your cat to play in the playground!",
       "location": "DUBAI!",
       "price": 7.99,
-      "image": "C://Users/hp/firsttask-git/catimg.jpg",
+      "image": "C://Users/hp/LabTaskIndivi/images/catimg1.jpg",
       "availableInventory": 11,
       "rating": 5
     },
     
     {
       "id": 1005,
-      "subject": "Laser Pointer",
-      "description": "Drive your cat crazy with this amazing product.",
+      "subject": "Pro Plan Sterilised",
+      "description": "Pro Plan Sterilised Optidigest - Chicken for Adult Cat (10kg).!",
       "location": "DUBAI!",
       "price": 49.99,
-      "image": "C://Users/hp/firsttask-git/catimg.jpg",
+      "image": "C://Users/hp/LabTaskIndivi/images/proplan.png",
       "availableInventory": 25,
       "rating": 4
     },
     {
+
         "id": 1006,
-        "subject": "Whiskas Chicken in Gravy Cat Food ",
+        "subject": "Whiskas Chicken",
         "description": "Actual product may slightly vary from the catalog due to product manufacturing",
         "location": "DUBAI!",
         "price": 7.99,
@@ -62,41 +63,41 @@ let arr = [
       },
       {
         "id": 1007,
-        "subject": "Cat House",
-        "description": "A place for your cat to play!",
+        "subject": "Arden Grange",
+        "description": "Grain Free Sensitive Adult Cat (8kg)!",
         "location": "AJMAN",
         "price": 7.99,
-        "image": "C://Users/hp/firsttask-git/catimg.jpg",
+        "image": "C://Users/hp/LabTaskIndivi/images/grainfree.png",
         "availableInventory": 11,
         "rating": 5
       },
       {
         "id": 1008,
-        "subject": "Cat House",
-        "description": "A place for your cat to play!",
+        "subject": "Cat Dreamies Cat Treats",
+        "description": "A place for your cat to Dreamies Cat Treats, Chicken, 60 gm!",
         "location": "SHARJA!",
         "price": 7.99,
-        "image": "C://Users/hp/firsttask-git/catimg.jpg",
+        "image": "C://Users/hp/LabTaskIndivi/images/Dreamies.jpg",
         "availableInventory": 11,
         "rating": 5
       },
       {
         "id": 1009,
-        "subject": "Cat House",
-        "description": "A place for your cat to play!",
+        "subject": "Purina Felix Doubly",
+        "description": "Purina Felix Doubly Delicious Fish Selection in Jelly Wet Cat Food Box 100g (12 Pouches)!",
         "location": "ALAIN",
         "price": 7.99,
-        "image": "C://Users/hp/firsttask-git/catimg.jpg",
+        "image": "C://Users/hp/LabTaskIndivi/images/felix.png",
         "availableInventory": 11,
         "rating": 5
       },
       {
         "id": 1010,
-        "subject": "zza",
-        "description": "A place for your cat to play!",
+        "subject": "MONELLO CAT FOOD",
+        "description": "MONELLO CAT FOOD SALMON,TUNA AND CHICKEN 1KG!",
         "location": "AJMAN",
         "price": 7.99,
-        "image": "C://Users/hp/firsttask-git/catimg.jpg",
+        "image": "C://Users/hp/LabTaskIndivi/images/monello.jpg",
         "availableInventory": 11,
         "rating": 5
       }
