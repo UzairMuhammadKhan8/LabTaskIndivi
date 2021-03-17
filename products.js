8aff19f18b55ec7e86d8fd84cfdf7b5a83e5c5f8
your <UK108@live.mdx.ac.uk>
2021-03-17 20:12:58 +0400

    first commit

---
+++ products.js
@@ -0,0 +1,93 @@
+let arr = [
+    {
+      "id": 1001,
+      "title": "Cat Food ddd",
+      "description": "A 25 pound bag of <em>irresistible</em>, organic goodness for your cat.",
+      "price": 20.00,
+      "image": "C://Users/hp/labtask/images/catimg.jpg",
+      "availableInventory": 10,
+      "rating": 2
+    },
+    {
+      "id": 1002,
+      "title": "Yarn",
+      "description": "Yarn your cat can play with for a very <strong>long</strong> time!",
+      "price": 2.99,
+      "image": "C://Users/hp/firsttask-git/catimg.jpg",
+      "availableInventory": 7,
+      "rating": 3
+    },
+    {
+      "id": 1003,
+      "title": "Kitty Litter",
+      "description": "Premium kitty litter for your cat.",
+      "price": 11.00,
+      "image": "C://Users/hp/firsttask-git/catimg.jpg",
+      "availableInventory": 99,
+      "rating": 4
+    },
+    {
+      "id": 1004,
+      "title": "Cat House",
+      "description": "A place for your cat to play!",
+      "price": 7.99,
+      "image": "C://Users/hp/firsttask-git/catimg.jpg",
+      "availableInventory": 11,
+      "rating": 5
+    },
+    
+    {
+      "id": 1005,
+      "title": "Laser Pointer",
+      "description": "Drive your cat crazy with this <em>amazing</em> product.",
+      "price": 49.99,
+      "image": "C://Users/hp/firsttask-git/catimg.jpg",
+      "availableInventory": 25,
+      "rating": 4
+    },
+    {
+        "id": 1006,
+        "title": "Cat House",
+        "description": "A place for your cat to play!",
+        "price": 7.99,
+        "image": "C://Users/hp/firsttask-git/catimg.jpg",
+        "availableInventory": 11,
+        "rating": 5
+      },
+      {
+        "id": 1007,
+        "title": "Cat House",
+        "description": "A place for your cat to play!",
+        "price": 7.99,
+        "image": "C://Users/hp/firsttask-git/catimg.jpg",
+        "availableInventory": 11,
+        "rating": 5
+      },
+      {
+        "id": 1008,
+        "title": "Cat House",
+        "description": "A place for your cat to play!",
+        "price": 7.99,
+        "image": "C://Users/hp/firsttask-git/catimg.jpg",
+        "availableInventory": 11,
+        "rating": 5
+      },
+      {
+        "id": 1009,
+        "title": "Cat House",
+        "description": "A place for your cat to play!",
+        "price": 7.99,
+        "image": "C://Users/hp/firsttask-git/catimg.jpg",
+        "availableInventory": 11,
+        "rating": 5
+      },
+      {
+        "id": 1010,
+        "title": "Cat House",
+        "description": "A place for your cat to play!",
+        "price": 7.99,
+        "image": "C://Users/hp/firsttask-git/catimg.jpg",
+        "availableInventory": 11,
+        "rating": 5
+      }
+  ]
